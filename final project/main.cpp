886d424c1e6738660ef376d4a6d1f3c204a75ce2
蔡博文 <caibowen@0587398009.wireless.umich.net>
2014-11-29 11:46:52 -0500

    Initial Commit

---
+++ final project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  final project
+//
+//  Created by 蔡博文 on 14-11-29.
+//  Copyright (c) 2014年 Bowen Cai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
